ee986b71705a92458fe1c4d0a09cc8270107c48b
2zzzyoung <mzooyoungn@gmail.com>
2023-02-25 23:58:22 +0900

    mouseEvent

--- basic/src/AppXY.jsx
+++ basic/src/AppXY.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from "react";
 import "./AppXY.css";
-import XY from "./components/XY";
 
 export default function AppXY() {
-  const [xmouse, setXmouse] = useState("");
-  const [ymouse, setYmouse] = useState("");
-
-  const handleMouseMove = (event) => {
-    setXmouse(event.clientX);
-    setYmouse(event.clientY);
-  };
+  // const [x, setX] = useState("");
+  // const [y, setY] = useState("");
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   return (
-    <div className="container" onMouseMove={(e) => handleMouseMove(e)}>
-      <XY className="pointer" xmouse={xmouse} ymouse={ymouse} />
+    <div
+      className="container"
+      onMouseMove={(e) => {
+        setPosition({ x: e.clientX, y: e.clientY });
+        console.log(e.clientX, e.clientY);
+        // 수평으로만 이동 가능하게 하려면
+        // setPosition((prev) => ({ ...prev, x: e.clientX }));
+      }}
+    >
+      {/* <XY className="pointer" x={x} y={y} /> */}
+      <div
+        className="pointer"
+        style={{ transform: `translate(${position.x}px), ${position.y}px` }}
+      />
     </div>
   );
 }
