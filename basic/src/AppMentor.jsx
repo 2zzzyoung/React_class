53caa8790135c676c44cd92480b9c7066636c723
2zzzyoung <mzooyoungn@gmail.com>
2023-02-27 23:05:34 +0900

    배열 상태 관리

--- basic/src/AppMentor.jsx
+++ basic/src/AppMentor.jsx
@@ -3,17 +3,17 @@ import { useState } from "react";
 
 export default function AppMentor() {
   const [person, setPerson] = useState({
-    name: "몽몽이",
+    name: "주영",
     title: "개발자",
     mentor: {
-      name: "주영",
+      name: "밥",
       title: "시니어개발자",
     },
   });
   return (
     <div>
       <h1>
-        {person.name}는 {person.title}
+        {person.name}은 {person.title}
       </h1>
       <p>
         {person.name}의 멘토는 {person.mentor.name} ({person.mentor.title})
@@ -43,4 +43,3 @@ export default function AppMentor() {
     </div>
   );
 }
-// setPerson에 새로운 값을 넣어주기
